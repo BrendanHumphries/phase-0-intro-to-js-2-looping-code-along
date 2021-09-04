c7c2738738a7d94f084ee4aead3fb61ad60d9831
Brendan Humphries <brendan.b.humphries@gmail.com>
2021-09-04 12:09:32 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    const messages = [];
+    for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages
+}
+
+function countDown(count) {
+    while (count > -1) {
+        console.log(count);
+        count--;
+    }
+}
